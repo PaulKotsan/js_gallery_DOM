d806527b334868dd60b1e5da5b399896ee7d2e79
Pavlo Kotsan <paul@Pavlos-MacBook-Pro.local>
2024-12-31 16:54:38 -0500

    make build

---
+++ index.74b409e2.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var n=e.target.closest(".list-item__link").href;t.src=n});
+//# sourceMappingURL=index.74b409e2.js.map
